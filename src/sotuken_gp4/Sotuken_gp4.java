2b2d9c8a6524642ed3493aaa211717250fe8b407
15024102 <15024102@mmm.muroran-it.ac.jp>
2018-12-14 17:36:42 +0900

    プログラム修正

--- src/sotuken_gp4/Sotuken_gp4.java
+++ src/sotuken_gp4/Sotuken_gp4.java
@@ -15,7 +15,56 @@ public class Sotuken_gp4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int x = 10;
+        int y = 2;
+        
+        System.out.println("x=" + x + ", y=" + y);
+        System.out.println("x＋y=" + sum(x,y));
+        System.out.println("x－y=" + difference(x,y));
+        System.out.println("x×y=" + product(x,y));
+        System.out.println("x÷y=" + quotient(x,y));
+        System.out.println();
+        primeNumber(); //1から100までの素数を表示
+    }
+    
+    //足し算
+    public static int sum(int x, int y){
+        return x + y;
+    }
+    
+    //引き算
+    public static int difference(int x, int y){
+        return x - y;
+    }
+    
+    //掛け算
+    public static int product(int x, int y){
+        return x * y;
+    }
+    
+    //割り算
+    public static int quotient(int x, int y){
+        return x / y;
+    }
+    
+    //1から100までの素数を表示する
+    public static void primeNumber(){
+        boolean flag = false;
+        
+        System.out.println("1から100までの素数を表示");
+        for(int i=2; i<=100; i++){
+            for(int j=2; j<i; j++){
+                if(i%j == 0){
+                    flag = true;
+                }
+            }
+            
+            if(flag == false){
+                System.out.println(i);
+            }
+
+            flag = false;
+        }
     }
     
 }
