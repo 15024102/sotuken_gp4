5d60356704473c490d5459e7bb95c2dbb26f7085
15024015-test1 <m-15024015-test@outlook.jp>
2018-12-18 18:18:29 +0900

    kashiki

--- src/sotuken_gp4/Sotuken_gp4.java
+++ src/sotuken_gp4/Sotuken_gp4.java
@@ -17,7 +17,7 @@ public class Sotuken_gp4 {
     public static void main(String[] args) {
         int x = 10;
         int y = 2;
-        
+        System.out.println("kashiki");
         System.out.println("x=" + x + ", y=" + y);
         System.out.println("x＋y=" + sum(x,y));
         System.out.println("x－y=" + difference(x,y));
