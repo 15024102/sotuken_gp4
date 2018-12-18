15e7506c325f017a8fcefaed0f02c5bbc530db99
15024040-test2 <mmm15024040@outlook.jp>
2018-12-18 19:00:30 +0900

    テスト

--- src/sotuken_gp4/Sotuken_gp4.java
+++ src/sotuken_gp4/Sotuken_gp4.java
@@ -19,8 +19,7 @@ public class Sotuken_gp4 {
         int y = 2;
         int n = 0;
         double answer = 0;
-        System.out.println("Error");
-        System.out.println("kashiki");
+
         System.out.println("x=" + x + ", y=" + y);
         System.out.println("x＋y=" + sum(x,y));
         System.out.println("x－y=" + difference(x,y));
